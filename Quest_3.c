46432a3820a9822e946048c11239ac41c42f5169
Ashish Kumar <88038080+Ashishkumar06@users.noreply.github.com>
2021-10-10 06:43:37 +0530

    Add files via upload

---
+++ Quest_3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	printf("Enter first number");
+	scanf("%d",&i);
+	printf("Enter second number");
+	scanf("%d",&j);
+	k=i+j;
+	printf("Sum of the numbers-[%d]",k);
+	return 0;
+}
