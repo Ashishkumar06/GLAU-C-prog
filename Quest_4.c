ac9a783cb8f6bbe302ec46419ebbf418043b34a6
Ashish Kumar <88038080+Ashishkumar06@users.noreply.github.com>
2021-10-10 06:48:56 +0530

    Add files via upload

---
+++ Quest_4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float i,j,k;
+	printf("Enter numbers");
+	scanf("%f",&i);
+	printf("Enter second number");
+	scanf("%f",&j);
+	k=i*j;
+	printf("Multiply of numbers-[%f]",k);
+	return 0;
+}
